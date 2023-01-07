bc5417348b557eddab2c97f4f199ab845f1bdb91
Roman <vladimirovrm@gmail.com>
2023-01-07 19:53:15 +0300

    add svg component

--- components/Htag/Htag.tsx
+++ components/Htag/Htag.tsx
@@ -1,16 +1,13 @@
 import { HtagProps } from './Htag.props';
 import styles from './Htag.module.css';
-import cn from 'classnames';
+import { ReactNode } from 'react';
 
 export const Htag = ({ tag, children }: HtagProps): JSX.Element => {
-	switch (tag) {
-		case 'h1':
-			return <h1 className={styles.h1}>{children}</h1>;
-		case 'h2':
-			return <h2 className={styles.h2}>{children}</h2>;
-		case 'h3':
-			return <h3 className={styles.h3}>{children}</h3>;
-		default:
-			return <></>;
-	}
+	const getHeading = (children: ReactNode) => ({
+		h1: <h1 className={styles.h1}>{children}</h1>,
+		h2: <h2 className={styles.h2}>{children}</h2>,
+		h3: <h3 className={styles.h3}>{children}</h3>,
+	});
+
+	return getHeading(children)[tag];
 };
--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+module.exports = {
+	images: {
+		domains: ['courses-top.ru'],
+	},
+	webpack(config, options) {
+		config.module.rules.push({
+			loader: '@svgr/webpack',
+			issuer: /\.[jt]sx?$/,
+			options: {
+				prettier: false,
+				svgo: true,
+				svgoConfig: {
+					plugins: [
+						{
+							name: 'preset-default',
+							params: {
+								override: {
+									removeViewBox: false,
+								},
+							},
+						},
+					],
+				},
+				titleProp: true,
+			},
+			test: /\.svg$/,
+		});
 
-module.exports = nextConfig
+		return config;
+	},
+};
