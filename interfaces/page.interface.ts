0d591889cfe66aaa4afd8b906013f7f8d9f58954
Roman <vladimirovrm@gmail.com>
2023-01-10 17:24:41 +0300

    alias component

---
+++ interfaces/page.interface.ts
@@ -0,0 +1,39 @@
+export enum TopLevelCategory {
+	Courses,
+	Services,
+	Books,
+	Products,
+}
+
+export interface PageAdvantage {
+	_id: string;
+	title: string;
+	description: string;
+}
+
+export interface HhData {
+	_id: string;
+	count: number;
+	juniorSalary: number;
+	middleSalary: number;
+	seniorSalary: number;
+	updatedAt: Date;
+}
+
+export interface PageModel {
+	tags: string[];
+	_id: string;
+	secondCategory: string;
+	alias: string;
+	title: string;
+	category: string;
+	seoText?: string;
+	tagsTitle: string;
+	metaTitle: string;
+	metaDescription: string;
+	firstCategory: TopLevelCategory;
+	advantages?: PageAdvantage[];
+	createdAt: Date;
+	updatedAt: Date;
+	hh?: HhData;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
+import { GetStaticProps } from 'next';
 import { useState } from 'react';
+import axios from 'axios';
 
 import { Htag, Button, Paragraph, Tag, Rating } from '../components';
 import { withLayout } from '../layout/Layout';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
 	const [rating, setRating] = useState<number>(4);
 
 	return (
@@ -18,7 +21,34 @@ function Home(): JSX.Element {
 			<Paragraph size='p14'>Маленький</Paragraph>
 			<Tag size='s'>ghost</Tag>
 			<Rating rating={rating} isEditable setRating={setRating} />
+			<ul>
+				{menu.map((m) => (
+					<li key={m._id.secondCategory}> {m._id.secondCategory}</li>
+				))}
+			</ul>
 		</main>
 	);
 }
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(
+		process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+		{
+			firstCategory,
+		}
+	);
+	return {
+		props: {
+			menu,
+			firstCategory,
+		},
+	};
+};
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
+	
+}
