f812c570ea2722681b4b609ad9b8efb5d2f8cf76
Roman <vladimirovrm@gmail.com>
2023-01-10 13:05:30 +0300

    create withLayout hoc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
-import { Htag, Button, Paragraph, Tag, Rating } from '../components';
 
-export default function Home(): JSX.Element {
+import { Htag, Button, Paragraph, Tag, Rating } from '../components';
+import { withLayout } from '../layout/Layout';
+
+function Home(): JSX.Element {
 	const [rating, setRating] = useState<number>(4);
 
 	return (
-		<>
+		<main>
 			<Htag tag='h1'> Заголовок </Htag>
 			<Button appearance='primary' arrow='right'>
 				Кнопка
@@ -16,6 +18,7 @@ export default function Home(): JSX.Element {
 			<Paragraph size='p14'>Маленький</Paragraph>
 			<Tag size='s'>ghost</Tag>
 			<Rating rating={rating} isEditable setRating={setRating} />
-		</>
+		</main>
 	);
 }
+export default withLayout(Home);
