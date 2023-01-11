0667a8e1c6648eafe61739a641170e0d1d4edcb2
Roman <vladimirovrm@gmail.com>
2023-01-11 16:27:08 +0300

    changes app context

--- context/app.context.tsx
+++ context/app.context.tsx
@@ -1,5 +1,4 @@
 import { createContext, ReactNode, useState } from 'react';
-
 import { MenuItem } from '../interfaces/menu.interface';
 import { TopLevelCategory } from '../interfaces/page.interface';
 
