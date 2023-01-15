e3c693b30bbed8d0fa3c2ae5a1a244c23c69f5f9
Roman <vladimirovrm@gmail.com>
2023-01-15 19:35:26 +0300

    change name function

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -7,38 +7,54 @@ import { Card } from '../Card/Card';
 import { Rating } from '../Rating/Rating';
 import { Tag } from '../Tag/Tag';
 import { Button } from '../Button/Button';
-import { declOfNum, priceRu } from '../../helpers/helper';
+import { declinationOfWords, priceRu } from '../../helpers/helper';
 import { Divider } from '../Divider/Divider';
 
 import styles from './Product.module.css';
 
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
+	const {
+		image,
+		categories,
+		title,
+		price,
+		oldPrice,
+		credit,
+		description,
+		reviewAvg,
+		initialRating,
+		reviewCount,
+		advantages,
+		disadvantages,
+		characteristics,
+	} = product;
+
 	const [isReviewOpened, setIsReviewOpened] = useState<boolean>(false);
 
 	return (
 		<>
 			<Card className={styles.product}>
 				<div className={styles.logo}>
-					<Image src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} width={70} height={70} />
+					<Image src={process.env.NEXT_PUBLIC_DOMAIN + image} alt={title} width={70} height={70} />
 					<img />
 				</div>
-				<div className={styles.title}>{product.title}</div>
+				<div className={styles.title}>{title}</div>
 				<div className={styles.price}>
-					{priceRu(product.price)}
-					{product.oldPrice && (
+					{priceRu(price)}
+					{oldPrice && (
 						<Tag className={styles.oldPrice} color='green'>
-							{priceRu(product.price - product.oldPrice)}
+							{priceRu(price - oldPrice)}
 						</Tag>
 					)}
 				</div>
 				<div className={styles.credit}>
-					{priceRu(product.credit)}/<span className={styles.month}>мес</span>
+					{priceRu(credit)}/<span className={styles.month}>мес</span>
 				</div>
 				<div className={styles.rating}>
-					<Rating rating={product.reviewAvg ?? product.initialRating} />
+					<Rating rating={reviewAvg ?? initialRating} />
 				</div>
 				<div className={styles.tags}>
-					{product.categories.map((category) => (
+					{categories.map((category) => (
 						<Tag className={styles.category} color='ghost' key={category}>
 							{category}
 						</Tag>
@@ -47,12 +63,12 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
 				<div className={styles.priceTitle}>цена</div>
 				<div className={styles.creditTitle}>кредит</div>
 				<div className={styles.rateTitle}>
-					{product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}
+					{reviewCount} {declinationOfWords(reviewCount, ['отзыв', 'отзыва', 'отзывов'])}
 				</div>
 				<Divider className={styles.hr} />
-				<div className={styles.description}>{product.description}</div>
+				<div className={styles.description}>{description}</div>
 				<div className={styles.feature}>
-					{product.characteristics.map((characteristic) => (
+					{characteristics.map((characteristic) => (
 						<div className={styles.characteristics} key={characteristic.name}>
 							<span className={styles.characteristicsName}>{characteristic.name}</span>
 							<span className={styles.characteristicsDots}></span>
@@ -61,16 +77,16 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
 					))}
 				</div>
 				<div className={styles.advBlock}>
-					{product.advantages && (
+					{advantages && (
 						<div className={styles.advantages}>
 							<div className={styles.advTitle}>Преимущества</div>
-							<div>{product.advantages}</div>
+							<div>{advantages}</div>
 						</div>
 					)}
-					{product.disadvantages && (
+					{disadvantages && (
 						<div className={styles.disadvantages}>
 							<div className={styles.advTitle}>Недостатки</div>
-							<div>{product.disadvantages}</div>
+							<div>{disadvantages}</div>
 						</div>
 					)}
 				</div>
@@ -94,7 +110,7 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
 					[styles.closed]: !isReviewOpened,
 				})}
 			>
-			asdasdsadas
+				asdasdsadas
 			</Card>
 		</>
 	);
--- helpers/helper.tsx
+++ helpers/helper.tsx
@@ -23,7 +23,7 @@ export const priceRu = (price: number | undefined): string => {
 		.concat(' ₽');
 };
 
-export const declOfNum = (number: number, titles: [string, string, string]): string => {
+export const declinationOfWords = (number: number, titles: [string, string, string]): string => {
 	const cases = [2, 0, 1, 1, 2];
 	return titles[number % 100 > 4 && number % 100 < 20 ? 2 : cases[number % 10 < 5 ? number % 10 : 5]];
 };
