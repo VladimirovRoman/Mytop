132c7dce3d8a03661ecffd75a308879db2fc7e1b
Roman <vladimirovrm@gmail.com>
2023-01-16 17:37:50 +0300

    input change

--- components/Input/Input.props.ts
+++ components/Input/Input.props.ts
@@ -1,3 +1,6 @@
 import { InputHTMLAttributes, DetailedHTMLProps } from 'react';
+import { FieldError } from 'react-hook-form';
 
-export interface InputProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {}
+export interface InputProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+	error?: FieldError;
+}
--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -5,13 +5,18 @@ import { InputProps } from './Input.props';
 import styles from './Input.module.css';
 
 export const Input = forwardRef(
-	({ className, ...props }: InputProps, ref: ForwardedRef<HTMLInputElement>): JSX.Element => {
+	({ className, error, ...props }: InputProps, ref: ForwardedRef<HTMLInputElement>): JSX.Element => {
 		return (
-			<input
-				className={cn(className, styles.input)}
-				ref={ref}
-				{...props}
-			></input>
+			<div className={cn(className, styles.inputWrapper)}>
+				<input
+					className={cn(styles.input, {
+						[styles.error]: error,
+					})}
+					ref={ref}
+					{...props}
+				/>
+				{<span className={styles.errorMessage}>{error?.message}</span>}
+			</div>
 		);
 	}
 );
