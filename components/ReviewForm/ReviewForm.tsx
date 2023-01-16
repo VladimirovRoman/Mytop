dc1c5501688c61c5a95d8e6ff68a5f1fa8b37c8d
Roman <vladimirovrm@gmail.com>
2023-01-16 15:03:16 +0300

    change reviewform

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -1,27 +1,32 @@
-import { ReviewFormProps } from './ReviewForm.props';
-
 import cn from 'classnames';
 
+import { Input, Rating, Textarea, Button } from '../index';
+
+import CloseIcon from './close.svg';
+import { ReviewFormProps } from './ReviewForm.props';
 import styles from './ReviewForm.module.css';
-import { Input } from '../Input/Input';
-import { Rating } from '../Rating/Rating';
-import { Textarea } from '../Textarea/Textarea';
-import { Button } from '../Button/Button';
 
 export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps): JSX.Element => {
 	return (
-		<div className={cn(styles.reviewForm, className)} {...props}>
-			<Input />
-			<Input />
-			<div className={styles.rating}>
-				<span>Оценка:</span>
-				<Rating rating={0} />
+		<>
+			<div className={cn(styles.reviewForm, className)} {...props}>
+				<Input placeholder='имя' />
+				<Input placeholder='Заголовок отзыва' className={styles.title} />
+				<div className={styles.rating}>
+					<span>Оценка:</span>
+					<Rating rating={0} />
+				</div>
+				<Textarea placeholder='Текст отзыва' className={styles.description} />
+				<div className={styles.submit}>
+					<Button appearance='primary'>Отправить</Button>
+					<span className={styles.info}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
+				</div>
 			</div>
-			<Textarea className={styles.description}/>
-			<div className={styles.submit}>
-				<Button appearance='primary'>Отправить</Button>
-				<span>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
+			<div className={styles.success}>
+				<div className={styles.successTitle}>Ваш отзыв отправлен</div>
+				<div>Спасибо, ваш отзыв будет опубликован после проверки</div>
+				<CloseIcon className={styles.close}/>
 			</div>
-		</div>
+		</>
 	);
 };
