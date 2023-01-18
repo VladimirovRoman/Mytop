7c28d4fd4e1170595fd79464921863982452a894
Roman <vladimirovrm@gmail.com>
2023-01-18 20:38:15 +0300

    add meta tags

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -1,88 +1,102 @@
-import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
-import { ParsedUrlQuery } from "querystring";
-import axios from "axios";
+import Head from 'next/head';
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
+import { ParsedUrlQuery } from 'querystring';
+import axios from 'axios';
 
-import { withLayout } from "../../layout/Layout";
-import { MenuItem } from "../../interfaces/menu.interface";
-import { PageModel, TopLevelCategory } from "../../interfaces/page.interface";
-import { ProductModel } from "../../interfaces/product.interface";
-import { firstLevelMenu } from "../../helpers/helpers";
-import { TopPageComponent } from "../../page-components";
-import { API } from "../../helpers/api";
+import { withLayout } from '../../layout/Layout';
+import { MenuItem } from '../../interfaces/menu.interface';
+import { PageModel, TopLevelCategory } from '../../interfaces/page.interface';
+import { ProductModel } from '../../interfaces/product.interface';
+import { firstLevelMenu } from '../../helpers/helpers';
+import { TopPageComponent } from '../../page-components';
+import { API } from '../../helpers/api';
 
 function TopPage({ firstCategory, page, products }: TopPageProps): JSX.Element {
-  return <TopPageComponent firstCategory={firstCategory} page={page} products={products} />;
+	return (
+		<>
+			<Head>
+				<title>{page.metaTitle}</title>
+				<meta name='description' content={page.metaDescription} />
+				<meta property={'og:title'} content={page.metaTitle} />
+				<meta property={'og:description'} content={page.metaDescription} />
+				<meta property={'og:type'} content='article' />
+			</Head>
+			<TopPageComponent firstCategory={firstCategory} page={page} products={products} />;
+		</>
+	);
 }
 
 export default withLayout(TopPage);
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  let paths: string[] = [];
-  for (const m of firstLevelMenu) {
-    const { data: menu } = await axios.post<MenuItem[]>(API.TopPage.find, {
-      firstCategory: m.id,
-    });
-    paths = paths.concat(menu.flatMap((menu) => menu.pages.map((page) => `/${m.route}/${page.alias}`)));
-  }
+	let paths: string[] = [];
+	for (const m of firstLevelMenu) {
+		const { data: menu } = await axios.post<MenuItem[]>(API.TopPage.find, {
+			firstCategory: m.id,
+		});
+		paths = paths.concat(
+			menu.flatMap((menu) => menu.pages.map((page) => `/${m.route}/${page.alias}`))
+		);
+	}
 
-  return {
-    paths,
-    fallback: true,
-  };
+	return {
+		paths,
+		fallback: true,
+	};
 };
 
 export const getStaticProps: GetStaticProps<TopPageProps> = async ({
-  params,
+	params,
 }: GetStaticPropsContext<ParsedUrlQuery>) => {
-  if (!params) {
-    return {
-      notFound: true,
-    };
-  }
+	if (!params) {
+		return {
+			notFound: true,
+		};
+	}
 
-  const firstCategoryItem = firstLevelMenu.find((m) => m.route === params.type);
-  if (!firstCategoryItem) {
-    return {
-      notFound: true,
-    };
-  }
+	const firstCategoryItem = firstLevelMenu.find((m) => m.route === params.type);
+	if (!firstCategoryItem) {
+		return {
+			notFound: true,
+		};
+	}
 
-  try {
-    const { data: menu } = await axios.post<MenuItem[]>(API.TopPage.find, {
-      firstCategory: firstCategoryItem.id,
-    });
+	try {
+		const { data: menu } = await axios.post<MenuItem[]>(API.TopPage.find, {
+			firstCategory: firstCategoryItem.id,
+		});
 
-    if (menu.length == 0) {
-      return {
-        notFound: true,
-      };
-    }
+		if (menu.length == 0) {
+			return {
+				notFound: true,
+			};
+		}
 
-    const { data: page } = await axios.get<PageModel>(API.TopPage.byAlias + params.alias);
+		const { data: page } = await axios.get<PageModel>(API.TopPage.byAlias + params.alias);
 
-    const { data: products } = await axios.post<ProductModel[]>(API.product.find, {
-      category: page.category,
-      limit: 10,
-    });
+		const { data: products } = await axios.post<ProductModel[]>(API.product.find, {
+			category: page.category,
+			limit: 10,
+		});
 
-    return {
-      props: {
-        menu,
-        firstCategory: firstCategoryItem.id,
-        page,
-        products,
-      },
-    };
-  } catch {
-    return {
-      notFound: true,
-    };
-  }
+		return {
+			props: {
+				menu,
+				firstCategory: firstCategoryItem.id,
+				page,
+				products,
+			},
+		};
+	} catch {
+		return {
+			notFound: true,
+		};
+	}
 };
 
 interface TopPageProps extends Record<string, unknown> {
-  menu: MenuItem[];
-  firstCategory: TopLevelCategory;
-  page: PageModel;
-  products: ProductModel[];
+	menu: MenuItem[];
+	firstCategory: TopLevelCategory;
+	page: PageModel;
+	products: ProductModel[];
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,25 @@
-import Head from "next/head";
-import type { AppProps } from "next/app";
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
 
-import "../styles/globals.css";
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return (
-    <>
-      <Head>
-        <title>MyTop - наш лучший топ</title>
-        <link key={1} rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+export default function App({ Component, pageProps, router }: AppProps): JSX.Element {
+	return (
+		<>
+			<Head>
+				<title>MyTop - наш лучший топ</title>
+				<link key={1} rel='icon' href='/favicon.ico' />
+				<link rel='preconnect' href='https://fonts.googleapis.com' />
+				<link rel='preconnect' href='https://fonts.gstatic.com' />
+				<link
+					href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap'
+					rel='stylesheet'
+				></link>
+				<meta property={'og:url'} content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+				<meta property={'og:locale'} content='ru_RU' />
+				<meta property={'og:type'} content='ru_RU' />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
