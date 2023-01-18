cc9ab06c08542c3d13c74597a454bb99332d1384
Roman <vladimirovrm@gmail.com>
2023-01-18 21:47:43 +0300

    add 404 and 500

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -10,22 +10,27 @@ import { ProductModel } from '../../interfaces/product.interface';
 import { firstLevelMenu } from '../../helpers/helpers';
 import { TopPageComponent } from '../../page-components';
 import { API } from '../../helpers/api';
+import { Error404 } from '../404';
 
 function TopPage({ firstCategory, page, products }: TopPageProps): JSX.Element {
+	if (!page || !products) {
+		return <Error404 />;
+	}
+
 	return (
 		<>
-			{page && products && (
-				<>
-					<Head>
-						<title>{page.metaTitle}</title>
-						<meta name='description' content={page.metaDescription} />
-						<meta property={'og:title'} content={page.metaTitle} />
-						<meta property={'og:description'} content={page.metaDescription} />
-						<meta property={'og:type'} content='article' />
-					</Head>
-					<TopPageComponent firstCategory={firstCategory} page={page} products={products} />;
-				</>
-			)}
+			(
+			<>
+				<Head>
+					<title>{page.metaTitle}</title>
+					<meta name='description' content={page.metaDescription} />
+					<meta property={'og:title'} content={page.metaTitle} />
+					<meta property={'og:description'} content={page.metaDescription} />
+					<meta property={'og:type'} content='article' />
+				</Head>
+				<TopPageComponent firstCategory={firstCategory} page={page} products={products} />;
+			</>
+			)
 		</>
 	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,59 @@
-import { GetStaticProps } from "next";
-import { useState } from "react";
-import axios from "axios";
+import { GetStaticProps } from 'next';
+import { useState } from 'react';
+import axios from 'axios';
 
-import { Htag, Button, Paragraph, Tag, Rating, Input, Textarea } from "../components";
-import { withLayout } from "../layout/Layout";
-import { MenuItem } from "../interfaces/menu.interface";
-import { API } from "../helpers/api";
-
+import { Htag, Button, Paragraph, Tag, Rating, Input, Textarea } from '../components';
+import { withLayout } from '../layout/Layout';
+import { MenuItem } from '../interfaces/menu.interface';
+import { API } from '../helpers/api';
 
 function Home({ menu }: HomeProps): JSX.Element {
-  const [rating, setRating] = useState<number>(4);
-  return (
-    <>
-      <Htag tag="h1"> Header </Htag>
-      <Button appearance="primary" arrow="right">
-        Кнопка
-      </Button>
-      <Button appearance="ghost" arrow="right">
-        Кнопка
-      </Button>
-      <Paragraph size="p14">Маленький</Paragraph>
-      <Paragraph size="p16">Средний</Paragraph>
-      <Paragraph size="p18">Большой</Paragraph>
-      <Tag size="s">ghost</Tag>
-      <Tag size="m" color="red">
-        red
-      </Tag>
-      <Tag size="s" color="green">
-        green
-      </Tag>
-      <Tag color="primary">lol</Tag>
-      <Rating rating={4} isEditable />
-      <Rating rating={rating} isEditable setRating={setRating} />
-      <Input placeholder="Имя"/>
-      <Textarea placeholder="Текст отзыва"/>
-    </>
-  );
+	const [rating, setRating] = useState<number>(4);
+
+	return (
+		<>
+			<Htag tag='h1'> Header </Htag>
+			<Button appearance='primary' arrow='right'>
+				Кнопка
+			</Button>
+			<Button appearance='ghost' arrow='right'>
+				Кнопка
+			</Button>
+			<Paragraph size='p14'>Маленький</Paragraph>
+			<Paragraph size='p16'>Средний</Paragraph>
+			<Paragraph size='p18'>Большой</Paragraph>
+			<Tag size='s'>ghost</Tag>
+			<Tag size='m' color='red'>
+				red
+			</Tag>
+			<Tag size='s' color='green'>
+				green
+			</Tag>
+			<Tag color='primary'>lol</Tag>
+			<Rating rating={4} isEditable />
+			<Rating rating={rating} isEditable setRating={setRating} />
+			<Input placeholder='Имя' />
+			<Textarea placeholder='Текст отзыва' />
+		</>
+	);
 }
 
 export default withLayout(Home);
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-  const firstCategory = 0;
-  const { data: menu } = await axios.post<MenuItem[]>(API.TopPage.find, {
-    firstCategory,
-  });
-  return {
-    props: {
-      menu,
-      firstCategory,
-    },
-  };
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(API.TopPage.find, {
+		firstCategory,
+	});
+	return {
+		props: {
+			menu,
+			firstCategory,
+		},
+	};
 };
 
 interface HomeProps extends Record<string, unknown> {
-  menu: MenuItem[];
-  firstCategory: number;
+	menu: MenuItem[];
+	firstCategory: number;
 }
