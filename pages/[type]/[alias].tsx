2423ed8c5efdc7c0caeb02b185abfb04a0de97fb
Roman <vladimirovrm@gmail.com>
2023-01-18 20:56:07 +0300

    fix

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -15,10 +15,10 @@ function TopPage({ firstCategory, page, products }: TopPageProps): JSX.Element {
 	return (
 		<>
 			<Head>
-				<title>{page.metaTitle}</title>
-				<meta name='description' content={page.metaDescription} />
-				<meta property={'og:title'} content={page.metaTitle} />
-				<meta property={'og:description'} content={page.metaDescription} />
+				<title>{page?.metaTitle}</title>
+				<meta name='description' content={page?.metaDescription} />
+				<meta property={'og:title'} content={page?.metaTitle} />
+				<meta property={'og:description'} content={page?.metaDescription} />
 				<meta property={'og:type'} content='article' />
 			</Head>
 			<TopPageComponent firstCategory={firstCategory} page={page} products={products} />;
