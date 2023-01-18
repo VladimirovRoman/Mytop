95ca8b23a06083d05b04049f6fdee6e221a732f5
Roman <vladimirovrm@gmail.com>
2023-01-18 14:31:29 +0300

    add aria-label attributes

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,44 +1,45 @@
-import { useRouter } from "next/router";
-import { useState } from "react";
-import cn from "classnames";
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import cn from 'classnames';
 
-import { SearchProps } from "./Search.props";
-import styles from "./Search.module.css";
-import { Input } from "../Input/Input";
-import { Button } from "../Button/Button";
-import SearchIcon from "./search_icon.svg";
+import { SearchProps } from './Search.props';
+import styles from './Search.module.css';
+import { Input } from '../Input/Input';
+import { Button } from '../Button/Button';
+import SearchIcon from './search_icon.svg';
 
 export const Search = ({ className, ...props }: SearchProps): JSX.Element => {
-  const [search, setSearch] = useState<string>("");
-  const router = useRouter();
+	const [search, setSearch] = useState<string>('');
+	const router = useRouter();
 
-  const goToSearch = () => {
-    router.push({
-      pathname: "/search",
-      query: {
-        q: search,
-      },
-    });
-  };
+	const goToSearch = () => {
+		router.push({
+			pathname: '/search',
+			query: {
+				q: search,
+			},
+		});
+	};
 
-  const handleKeyDown = (event: React.KeyboardEvent) => {
-    if (event.key === "Enter") {
-      goToSearch();
-    }
-  };
+	const handleKeyDown = (event: React.KeyboardEvent) => {
+		if (event.key === 'Enter') {
+			goToSearch();
+		}
+	};
 
-  return (
-    <div className={cn(className, styles.search)} {...props}>
-      <Input
-        className={styles.input}
-        placeholder="Поиск..."
-        value={search}
-        onChange={(event) => setSearch(event.target.value)}
-        onKeyDown={handleKeyDown}
-      />
-      <Button appearance="primary" className={styles.button} onClick={goToSearch}>
-        <SearchIcon />
-      </Button>
-    </div>
-  );
+	return (
+		<div className={cn(className, styles.search)} {...props}>
+			<Input
+				className={styles.input}
+				placeholder='Поиск...'
+				value={search}
+				onChange={(event) => setSearch(event.target.value)}
+				onKeyDown={handleKeyDown}
+				aria-label='Искать по сайту'
+			/>
+			<Button appearance='primary' className={styles.button} onClick={goToSearch}>
+				<SearchIcon />
+			</Button>
+		</div>
+	);
 };
--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,60 +1,60 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer } from 'react';
 
-import { Benefits, HhData, Htag, Product, Sort, Tag } from "../../components";
-import { TopPageComponentProps } from "./TopPageComponent.props";
-import styles from "./TopPageComponent.module.css";
-import { TopLevelCategory } from "../../interfaces/page.interface";
-import { SortEnum } from "../../components/Sort/Sort.props";
-import { sortReducer } from "./sort.reducer";
+import { Benefits, HhData, Htag, Product, Sort, Tag } from '../../components';
+import { TopPageComponentProps } from './TopPageComponent.props';
+import styles from './TopPageComponent.module.css';
+import { TopLevelCategory } from '../../interfaces/page.interface';
+import { SortEnum } from '../../components/Sort/Sort.props';
+import { sortReducer } from './sort.reducer';
 
 export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
-  const [{ products: sortedProducts, sort }, dispathSort] = useReducer(sortReducer, {
-    products,
-    sort: SortEnum.Rating,
-  });
+	const [{ products: sortedProducts, sort }, dispathSort] = useReducer(sortReducer, {
+		products,
+		sort: SortEnum.Rating,
+	});
 
-  const setSort = (sort: SortEnum) => {
-    dispathSort({ type: sort });
-  };
+	const setSort = (sort: SortEnum) => {
+		dispathSort({ type: sort });
+	};
 
-  useEffect(() => {
-    dispathSort({ type: "reset", initialState: products });
-  }, [products]);
+	useEffect(() => {
+		dispathSort({ type: 'reset', initialState: products });
+	}, [products]);
 
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.title}>
-        <Htag tag="h1">{page?.title}</Htag>
-        {products && (
-          <Tag color="gray" size="m">
-            {products.length}
-          </Tag>
-        )}
-        <Sort sort={sort} setSort={setSort} />
-      </div>
-      <div>{sortedProducts && sortedProducts.map((p) => <Product layout key={p.title} product={p} />)}</div>
-      <div className={styles.hhTitle}>
-        <Htag tag="h2">Вакансии - {page?.category}</Htag>
-        <Tag color="red" size="m">
-          hh.ru
-        </Tag>
-      </div>
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.title}>
+				<Htag tag='h1'>{page?.title}</Htag>
+				{products && (
+					<Tag color='gray' size='m' aria-label={products.length + 'элементов'}>
+						{products.length}
+					</Tag>
+				)}
+				<Sort sort={sort} setSort={setSort} />
+			</div>
+			<div>{sortedProducts && sortedProducts.map((p) => <Product layout key={p.title} product={p} />)}</div>
+			<div className={styles.hhTitle}>
+				<Htag tag='h2'>Вакансии - {page?.category}</Htag>
+				<Tag color='red' size='m'>
+					hh.ru
+				</Tag>
+			</div>
 
-      {firstCategory === TopLevelCategory.Courses && page.hh && <HhData {...page?.hh} />}
+			{firstCategory === TopLevelCategory.Courses && page.hh && <HhData {...page?.hh} />}
 
-      {!!page?.advantages.length && (
-        <>
-          <Htag tag="h2">Преимущества</Htag>
-          <Benefits advantages={page?.advantages} />
-        </>
-      )}
-      {page?.seoText && <div className={styles.seo} dangerouslySetInnerHTML={{ __html: page?.seoText }} />}
-      <Htag tag="h2">Получаемые навыки</Htag>
-      {page?.tags.map((tag) => (
-        <Tag key={tag} color="primary">
-          {tag}
-        </Tag>
-      ))}
-    </div>
-  );
+			{!!page?.advantages.length && (
+				<>
+					<Htag tag='h2'>Преимущества</Htag>
+					<Benefits advantages={page?.advantages} />
+				</>
+			)}
+			{page?.seoText && <div className={styles.seo} dangerouslySetInnerHTML={{ __html: page?.seoText }} />}
+			<Htag tag='h2'>Получаемые навыки</Htag>
+			{page?.tags.map((tag) => (
+				<Tag key={tag} color='primary'>
+					{tag}
+				</Tag>
+			))}
+		</div>
+	);
 };
