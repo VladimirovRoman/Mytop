c1d318d3cdee90cba97454a14e66104798ba4192
Roman <vladimirovrm@gmail.com>
2023-01-18 13:05:32 +0300

    accessibility sorting

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -1,29 +1,30 @@
-import cn from "classnames";
+import { KeyboardEvent } from 'react';
+import cn from 'classnames';
 
-import { SortEnum, SortProps } from "./Sort.props";
-import styles from "./Sort.module.css";
-import SortIcon from "./sort_icon.svg";
+import { SortEnum, SortProps } from './Sort.props';
+import styles from './Sort.module.css';
+import SortIcon from './sort_icon.svg';
 
 export const Sort = ({ sort, setSort, className, ...props }: SortProps): JSX.Element => {
-  return (
-    <div className={cn(styles.sort, className)} {...props}>
-      <span
-        onClick={() => setSort(SortEnum.Rating)}
-        className={cn({
-          [styles.active]: sort === SortEnum.Rating,
-        })}
-      >
-        <SortIcon className={styles.sortIcon} /> По рейтингу
-      </span>
+	return (
+		<div className={cn(styles.sort, className)} {...props}>
+			<button
+				onClick={() => setSort(SortEnum.Rating)}
+				className={cn({
+					[styles.active]: sort === SortEnum.Rating,
+				})}
+			>
+				<SortIcon className={styles.sortIcon} /> По рейтингу
+			</button>
 
-      <span
-        onClick={() => setSort(SortEnum.Price)}
-        className={cn({
-          [styles.active]: sort === SortEnum.Price,
-        })}
-      >
-        <SortIcon className={styles.sortIcon} /> По цене
-      </span>
-    </div>
-  );
+			<button
+				onClick={() => setSort(SortEnum.Price)}
+				className={cn({
+					[styles.active]: sort === SortEnum.Price,
+				})}
+			>
+				<SortIcon className={styles.sortIcon} /> По цене
+			</button>
+		</div>
+	);
 };
